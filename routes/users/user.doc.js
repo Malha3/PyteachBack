a866a4186d4213a26cf5742abe8f13afe72887d1
Badis Meddouri <meddouri.badis@gmail.com>
2020-09-27 20:40:36 +0200

    APIDOC : user.doc.js

--- routes/users/user.doc.js
+++ routes/users/user.doc.js
@@ -7,7 +7,7 @@
  *
  *     body:
  *     {
- *      "username": "jajson",
+ *      "username": "Badis",
  *      "password": "my-super-secret-password"
  *     }
  *
@@ -50,5 +50,48 @@
  *
  *
  * @apiSuccess {Json} UserList  Json list of all Users
+ *
+ */
+
+/**
+ * @api {get} /users/current Current
+ * @apiName Current
+ * @apiGroup User
+ *
+ * @apiSuccess {Int} id id of the User.
+ * @apiSuccess {String} firstName  Firstname of the User.
+ * @apiSuccess {String} lastName  Lastname of the User.
+ * @apiSuccess {String} username  username of the User.
+ * @apiSuccess {String} role  role of the User.
+ * @apiSuccess {String} email  Lastname of the User.
+ *
+ */
+
+/**
+ * @api {get} /users/:id Request User information
+ * @apiName GetUser
+ * @apiGroup User
+ * @apiHeader {String} JWT access token.
+ *
+ * @apiParam {Number} id Users unique ID.
+ *
+ * @apiSuccess {Int} id id of the User.
+ * @apiSuccess {String} firstName  Firstname of the User.
+ * @apiSuccess {String} lastName  Lastname of the User.
+ * @apiSuccess {String} username  username of the User.
+ * @apiSuccess {String} role  role of the User.
+ * @apiSuccess {String} email  Lastname of the User.
+ *
+ */
+
+/**
+ * @api {delete} /users/:id Delete User
+ * @apiName DeleteUser
+ * @apiGroup User
+ * @apiHeader {String} JWT access token.
+ *
+ * @apiParam {Number} id Users unique ID.
+ *
+ * @apiSuccess {String} message  Delete confirmation
  *
  */
