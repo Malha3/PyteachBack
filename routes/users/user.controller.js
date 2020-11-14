ee0e5cbcd99903a370253b3f1ab693893a273208
Badis MEDDOURI <meddouri.badis@gmail.com>
2020-11-14 11:53:13 +0100

    Clean

--- routes/users/user.controller.js
+++ routes/users/user.controller.js
@@ -43,7 +43,9 @@ function registerSchema(req, res, next) {
 
 function register(req, res, next) {
     userService.create(req.body)
-        .then(() => res.json({ message: 'Registration successful' }))
+        .then(() => res.status(201).json({
+            message: 'Registration successful'
+        }))
         .catch(next);
 }
 
