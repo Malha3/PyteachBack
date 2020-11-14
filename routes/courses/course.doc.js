7b9e33fa94b87c9f49714b0c06d87fcc2e186d30
lhouraga <houraga1@gmail.com>
2020-11-14 17:03:56 +0100

    docCourse#1

---
+++ routes/courses/course.doc.js
@@ -0,0 +1,34 @@
+/**
+ * @api {post} /courses/ Create course
+ * @apiName Create
+ * @apiGroup Course
+ *
+ * @apiExample Example usage:
+ *
+ *     body:
+ *     {
+ *      "title": "Functions in language Python",
+ *      "description": "vous apprendrez à créer vos propres fonctions et procédure",
+ *      "slug": "",
+ *      "author_id": "26",
+ *      "tags": "",
+ *      "published": "False",
+ *      "id_cat": "001"
+ *     }
+ *
+ *
+ * @apiSuccess {String} message  Course created successfully 
+ *
+ */
+
+
+/**
+ * @api {get} /courses/ Get All Courses
+ * @apiName GetAll
+ * @apiGroup Course
+ * @apiHeader {String} JWT access token.
+ *
+ *
+ * @apiSuccess {Json} CourseList  Json list of all Courses
+ *
+ */
